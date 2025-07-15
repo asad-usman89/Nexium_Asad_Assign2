d2a536dd566473aec1d28665ad592954a26b4225
M Asad Ur Rehman Bin Usman <asad.usman252@gmail.com>
2025-07-15 22:23:21 +0500

    refactor: convert MongoDB connection from JavaScript to TypeScript

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,14 +1,18 @@
 import { MongoClient } from "mongodb";
 
+declare global {
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
+}
+
 if (!process.env.MONGODB_URI) {
   throw new Error("Please add your MongoDB URI to .env.local");
 }
 
-const uri = process.env.MONGODB_URI;
+const uri: string = process.env.MONGODB_URI;
 const options = {};
 
-let client;
-let clientPromise;
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
 
 if (!global._mongoClientPromise) {
   client = new MongoClient(uri, options);
